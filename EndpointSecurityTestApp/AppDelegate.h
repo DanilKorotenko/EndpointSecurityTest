403f7651594959c315d4b2d6c60e08bb59b6a43b
Danil Korotenko <danil.korotenko@gmail.com>
2020-05-25 16:43:03 +0300

    Test app was added.

---
+++ EndpointSecurityTestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EndpointSecurityTestApp
+//
+//  Created by Danil Korotenko on 5/25/20.
+//  Copyright © 2020 Timm Kandziora. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ EndpointSecurityTestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EndpointSecurityTestApp
+//
+//  Created by Danil Korotenko on 5/25/20.
+//  Copyright © 2020 Timm Kandziora. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
